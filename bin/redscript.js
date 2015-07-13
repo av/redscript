d33c482b9481afb848f0946d6d58faa19ceab5c1
Ivan Cherepanov <icharapanau@iba.by>
2015-07-13 09:02:55 +0100

    Moved to use your-script

--- bin/redscript.js
+++ bin/redscript.js
@@ -1,3 +1,34 @@
 #!/usr/bin/env node
 
-var redScript = require('../lib/index.js');
+var fs = require('fs');
+var translator = require('your-script');
+var args = process.argv.slice(2)
+var inputFile = args[0];
+var redscript = new translator({
+    lexemsFolder: './lexems'
+});
+
+var inputExt, outputExt;
+var inputType, outputType;
+
+function setOptions(inputFile) {
+    inputExt = path.extname(inputFile).substr(1);
+    inputFile = fs.readFileSync(inputFile);
+
+    if (inputExt === 'js') {
+        inputType = 'javascript';
+        outputType = 'redscript';
+        outputExt = 'rs';
+    } else if (inputExt === 'rs') {
+        inputType = 'redscript';
+        outputType = 'javascript';
+        outputExt = 'js';
+    }
+
+    var parsedSource = redscript.parse(inputFile, {
+        sourceSubset: inputType,
+        destinationSubset: outputType
+    });
+
+    fs.writeFileSync(inputFile.replace(inputExt, outputExt), parsedSource);
+}
